64d278784873a453814b89cc947e6ed2f6be3fcc
liudalao2003 <test@example.com>
2026-02-14 23:45:40 +0800

    修复中间件报错

--- middleware.ts
+++ middleware.ts
@@ -1,12 +1,18 @@
-import { clerkMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+// 定义需要保护的路由（比如只有接口生成才需要登录）
+const isProtectedRoute = createRouteMatcher(['/api/generate(.*)']);
+
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
+    "/((?!.*\\..*|_next).*)", 
+    "/", 
+    "/(api|trpc)(.*)"
   ],
 };
